1d826250c1fd7daa4fc452d78107296ba78f6cef
Martin <martin.grzeslowski@gmail.com>
2016-04-26 22:15:42 +0200

    ClojureScriptProcessor.groovy stub

---
+++ src/main/groovy/asset/pipeline/clj/ClojureScriptProcessor.groovy
@@ -0,0 +1,24 @@
+package asset.pipeline.clj
+
+
+import asset.pipeline.AssetFile
+import org.mozilla.javascript.Context
+import org.mozilla.javascript.Scriptable
+import asset.pipeline.AbstractProcessor
+import asset.pipeline.AssetCompiler
+
+class ClojureScriptProcessor  extends AbstractProcessor {
+	/**
+	 * Constructor for building a Processor
+	 *
+	 * @param precompiler - An Instance of the AssetCompiler class compiling the file or NULL for dev mode.
+	 */
+	ClojureScriptProcessor(AssetCompiler precompiler) {
+		super(precompiler)
+	}
+
+	@Override
+	String process(String inputText, AssetFile assetFile) {
+		return null
+	}
+}
