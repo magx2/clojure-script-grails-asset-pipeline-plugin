83b0a4e551ff70ead38e92692fc45f2a270cdf06
Martin <martin.grzeslowski@gmail.com>
2016-04-28 20:08:38 +0200

    valid package

--- src/main/groovy/asset/pipeline/clj/ClojureScriptProcessor.groovy
+++ src/main/groovy/asset/pipeline/clj/ClojureScriptProcessor.groovy
@@ -25,7 +25,7 @@ class ClojureScriptProcessor extends AbstractProcessor {
 
 		IFn emptyEnv = Clojure.var("cljs.analyzer.api", "empty-env")
 		IFn analyze = Clojure.var("cljs.analyzer.api", "analyze")
-		IFn emit = Clojure.var("cljs.compiler", "emit")
+		IFn emit = Clojure.var("cljs.compiler.api", "emit")
 
 		emit.invoke(
 				analyze.invoke(
