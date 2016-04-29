43a30838551555e3a642fc92c6b00c32fd235ed4
Martin <martin.grzeslowski@gmail.com>
2016-04-29 08:29:57 +0200

    cljs extension

--- src/main/groovy/asset/pipeline/clj/ClojureScriptAssetFile.groovy
+++ src/main/groovy/asset/pipeline/clj/ClojureScriptAssetFile.groovy
@@ -7,7 +7,7 @@ import java.util.regex.Pattern
 
 class ClojureScriptAssetFile extends AbstractAssetFile {
 	static final contentType = ['application/javascript','application/x-javascript','text/javascript']
-	static extensions = ['clj']
+	static extensions = ['clj', 'cljs']
 	static final String compiledExtension = 'js'
 	static processors = [ClojureScriptProcessor]
 	Pattern directivePattern = ~/(?m)#=(.*)/
